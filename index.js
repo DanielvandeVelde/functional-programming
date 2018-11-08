e53f95e1afabd9f15c867d5e4920c08cafc4b79b
Daniel van de Velde <31889309+DanielvandeVelde@users.noreply.github.com>
2018-11-08 23:49:01 +0100

    Functional
    Stopped using 8 functions and used .filter and .map instead

--- index.js
+++ index.js
@@ -14,87 +14,42 @@ const oba = new OBA({
 
 console.log("Hello there! I'm going to grab data from this website:")
 oba.getAll('search', {
-  q: 'groenten',
-  sort: 'title',
+  q: 'author:Stephen King',
   librarian: true,
   refine: true,
-  facet: 'type(book)&facet=language(dut)'
-}, 'df215')
-  .then(console.log("Data found! Let me do all of my amazing calculations..."))
+  facet: 'type(book)'
+}, 'marc')
   .then(res => {
     data.response = helper.getMainKey(res)
-    let response = letsGetThisPartyStarted(data);
-    data.response = "<h1>De pagina pagina</h1>"
-    data.response += response;
+    let response = sanitizeData(data.response);
+    data.response = response;
+    console.log("All done! I'm showing you the good stuff on localhost:3000")
   })
   .catch(err => {
-    if (data.response) {
-      data.response = `${err.response.status} ${err.response.statusText}. See terminal for more details.`
-      console.log(err, err.response.status, err.response.statusText)
-    } else {
-      data.response = 'Error: check terminal for details'
-      console.log(err)
-    }
-  })
+    console.log("API overloaded, please try again later.")
 
-function letsGetThisPartyStarted(data){
-  let oddElements = getOddElements(data.response);
-  let pageNumbers = parsePageNumbers(oddElements);
-  let averagePages = getStats(pageNumbers);
-  let summary =
-  "<p> Er zijn " + oddElements.length + " boeken die jouw zoekwoord bevatten!</p>" +
-  "<p>Het boek met het minste aantal pagina's heeft er " + averagePages.min + ".</p>" +
-  "<p>Het gemiddelde aantal pagina's per boek is " + averagePages.average + ".</p>" +
-  "<p>Het boek met het meeste aantal pagina's heeft er " + averagePages.max + ".</p>"
-  ;
 
-  console.log("All done! I'm showing you the good stuff on localhost:3000")
-  return summary
-}
+//Grab the needed properties from the elements.
+//sanatize them and convert to number if needed.
+function sanitizeData(data){
 
-function getOddElements(data) {
-  let odd = new Array;
+  let cleanData = data
+    .filter(element => element.df215 && element.df101 && element.df200)
+    .map(element => {
+      let newTitle = element.df200[0].df200[0]._
+      if (newTitle.startsWith("\r\n")) {
+        newTitle = newTitle.substr(4)
+        newTitle = newTitle.trim()
+        newTitle = ("Stephen King " + newTitle)
+      }
+      return {
+      pageNumber: Number(element.df215[0].df215[0]._.match(/\[?(\d+)\]?[\w\s]*(?:p|pagina's|bladen)/)[1]),
+      language: element.df101[0].df101[0]._.match(/\[(.*?)\]/)[1],
+      title: newTitle
+      }
+    })
 
-  //Omdat elementen dubbel staan pakken we alleen de oneven
-  data.forEach((x, i) => {
-    if(i % 2 == 0) {
-      return
-    } else {
-      odd.push(x);
-    }
-  });
-
-  return odd
-}
-
-function parsePageNumbers(data) {
-  let numbers = new Array;
-  let fail = 0;
-
-  data.forEach((x, i) => {
-    let number = x.substr(0, x.indexOf(' p'));
-    number = Number(number.split(" ").splice(-1)[0]);
-
-    if (isNaN(number) || number == 0) {
-      fail++
-    }else{
-      numbers.push(number);
-    }
-  });
-
-  if (fail) {console.log("I messed up the pagenumber for " + fail + " result(s) :^(")}
-  return numbers
-}
-
-function getStats(data) {
-  let sum = data.reduce((acc, cur) => cur += acc);
-  let average = sum / data.length;
-
-  return {
-    average: average.toFixed(2),
-    max: Math.max(...data),
-    min: Math.min(...data)
-  }
+    return cleanData
 }
 
 const app = express()
